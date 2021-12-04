b3fdafc7f11ac184364def81f512727963b7df38
Nikolai <nikolaichub.me@gmail.com>
2021-12-04 22:35:52 +0200

    tasks view

--- frontend/src/views/Tasks/Tasks.js
+++ frontend/src/views/Tasks/Tasks.js
@@ -1,10 +1,43 @@
 import './Tasks.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCalendarAlt, faPlusSquare } from '@fortawesome/free-solid-svg-icons';
 
 const Tasks = () => {
 
     return (
-        <div id="tasks">
-            Tasks
+        <div id="tasks" className="tasks__wrapper">
+            <div className="tasks__header">
+                <div className="header__filter">
+                    Sort by
+                </div>
+                <div className="header__action">
+                    <div className="header__new header__button">
+                        <span>Create Task</span>
+                        <FontAwesomeIcon className="header__icon" icon={faPlusSquare} />
+                    </div>
+                    <div className="header__generate header__button">
+                        <span>Generate Schedule</span>
+                        <FontAwesomeIcon className="header__icon" icon={faCalendarAlt} />
+                    </div>
+                </div>
+            </div>
+            <div className="tasks__body">
+                <div className="tasks__item"></div>
+                <div className="tasks__item"></div>
+                <div className="tasks__item"></div>
+                <div className="tasks__item"></div>
+                <div className="tasks__item"></div>
+                <div className="tasks__item"></div>
+                <div className="tasks__item"></div>
+                <div className="tasks__item"></div>
+                <div className="tasks__item"></div>
+                <div className="tasks__item"></div>
+                <div className="tasks__item"></div>
+                <div className="tasks__item"></div>
+                <div className="tasks__item"></div>
+                <div className="tasks__item"></div>
+                <div className="tasks__item"></div>
+            </div>
         </div>
     );
 };
