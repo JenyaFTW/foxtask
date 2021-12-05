46b29d45a11827cc729a80ad4f3bf882898b37b2
hack_er <70893070+Er4ik@users.noreply.github.com>
2021-12-05 06:23:09 +0200

    add guard on table

--- backend/src/table/table.controller.ts
+++ backend/src/table/table.controller.ts
@@ -1,10 +1,12 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from "@nestjs/common";
+import { Body, Controller, HttpCode, HttpStatus, Post, UseGuards } from "@nestjs/common";
+import { AuthService } from "src/auth/auth.service";
 import { TableService } from "./table.service";
 
 @Controller('table')
 export class TableController {
     constructor(private readonly tableService: TableService) {}
     @Post()
+    @UseGuards(AuthService)
     @HttpCode(HttpStatus.CREATED)
     async generateTimetable(@Body() table: number[][]): Promise<number[][]> {
         return this.tableService.genTimetable(table)
--- backend/src/table/table.module.ts
+++ backend/src/table/table.module.ts
@@ -1,5 +1,6 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { AuthService } from "src/auth/auth.service";
 import { GenTimetable } from "src/helper/helper.service";
 import { Task } from "src/task/task.entity";
 import { TableController } from "./table.controller";
@@ -7,7 +8,7 @@ import { TableService } from "./table.service";
 
 @Module({
     imports: [TypeOrmModule.forFeature([Task])],
-    providers: [TableService, GenTimetable],
+    providers: [TableService, GenTimetable, AuthService],
     controllers: [TableController]
 })
 export class TableModule {}
