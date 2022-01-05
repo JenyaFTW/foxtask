b1824b3a60bc7a7d3096b406245395bf0c221ed7
JenyaFTW <yyakovliev02@gmail.com>
2022-01-05 18:47:51 +0200

    swagger

--- backend/lib/server.js
+++ backend/lib/server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const path = require('path');
 
+const swaggerDocument = YAML.load(path.join(__dirname, '..', 'swagger.yml'));
 const apiRoute = require('../routes/api.route');
 const webRoute = require('../routes/web.route');
 
@@ -13,6 +17,7 @@ app.use(cors());
 app.use(helmet());
 app.use(morgan('dev'));
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api', apiRoute);
 app.use('/', webRoute);
 
