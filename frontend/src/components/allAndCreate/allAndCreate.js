bc6e24832183a2847bc586236a950ffb22022d79
Nikolai <nikolaichub.me@gmail.com>
2022-01-07 23:53:27 +0200

    main tasks info updated

--- frontend/src/components/allAndCreate/allAndCreate.js
+++ frontend/src/components/allAndCreate/allAndCreate.js
@@ -1,34 +1,33 @@
-import CreateTask from "../createTask/createTask.js";
+import CreateTask from '../createTask/createTask.js';
 import { useState, useEffect } from 'react';
 
-import "./allAndCreate.scss";
+import './allAndCreate.scss';
 
 const allAndCreate = () => {
-    const [isModal, setModal] = useState(false)
-    const onClose = () => setModal(false)
+  const [isModal, setModal] = useState(false);
+  const onClose = () => setModal(false);
 
-    const onKeydown = ({ key }) => {
-      switch (key) {
-        case 'Escape':
-          onClose();
-          break;
-      }
-    };
+  const onKeydown = ({ key }) => {
+    switch (key) {
+      case 'Escape':
+        onClose();
+        break;
+    }
+  };
 
-    useEffect(() => {
-      document.addEventListener('keydown', onKeydown)
-      return () => document.removeEventListener('keydown', onKeydown)
-    })
+  useEffect(() => {
+    document.addEventListener('keydown', onKeydown);
+    return () => document.removeEventListener('keydown', onKeydown);
+  });
 
-    return(
-        <div className="header__tasks">
-            <div className="count__tasks">2 Tasks 2 All-day events</div>
-            <button type="button"
-                    className="btn__create__tasks"
-                    onClick={() => setModal(true)}>Create Task</button>
-            <CreateTask  visible={isModal}
-                         onClose={onClose}/>
-        </div>
-    )
-}
+  return (
+    <div className="header__tasks">
+      <div className="count__tasks">2 Tasks 2 All-day events</div>
+      <button type="button" className="btn__create__tasks" onClick={() => setModal(true)}>
+        Create Task
+      </button>
+      <CreateTask visible={isModal} onClose={onClose} />
+    </div>
+  );
+};
 export default allAndCreate;
