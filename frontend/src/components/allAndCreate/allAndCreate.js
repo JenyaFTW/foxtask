5f1c5f1fbbcfdc015403b57462b0530db5448b21
Nikolai <nikolaichub.me@gmail.com>
2022-01-08 16:50:16 +0200

    ActionButton

--- frontend/src/components/allAndCreate/allAndCreate.js
+++ frontend/src/components/allAndCreate/allAndCreate.js
@@ -1,4 +1,5 @@
 import CreateTask from '../createTask/createTask.js';
+import ActionButton from '../actionButton/ActionButton.js';
 import { useState, useEffect } from 'react';
 
 import './allAndCreate.scss';
@@ -6,6 +7,7 @@ import './allAndCreate.scss';
 const allAndCreate = () => {
   const [isModal, setModal] = useState(false);
   const onClose = () => setModal(false);
+  const onOpen = () => setModal(true);
 
   const onKeydown = ({ key }) => {
     switch (key) {
@@ -23,11 +25,10 @@ const allAndCreate = () => {
   return (
     <div className="header__tasks">
       <div className="count__tasks">2 Tasks 2 All-day events</div>
-      <button type="button" className="btn__create__tasks" onClick={() => setModal(true)}>
-        Create Task
-      </button>
+      <ActionButton onClick={onOpen} />
       <CreateTask visible={isModal} onClose={onClose} />
     </div>
   );
 };
+
 export default allAndCreate;
